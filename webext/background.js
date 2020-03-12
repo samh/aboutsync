240b0bd561e82d737d6a4ac12218c43e92811e3b
Mark Hammond <mhammond@skippinet.com.au>
2020-03-12 18:00:48 +1100

    Add some diagnostics for people trying to test an unsigned copy

--- webext/background.js
+++ webext/background.js
@@ -1,5 +1,16 @@
 // onInstalled, but not onStartup, is called when the addon is installed.
 browser.runtime.onInstalled.addListener(() => {
+  if (!browser.aboutsync) {
+    // no browser.aboutsync almost certainly means Firefix didn't load our
+    // "experimental api", so make noise.
+    let msg = "\n\n***** NOTE: about:sync is going to fail to load ****\n" +
+              "If you are running this extension locally, it looks alot like you" +
+              " need to set the preference `extensions.experiments.enabled` to `true`" +
+              " before things will work for you. Note that this preference can" +
+              " only be changed in Nightly\n\n";
+    console.error(msg);
+    dump(msg);
+  }
   browser.aboutsync.startup();
 });
 // onStartup is called at browser startup if the addon is already installed.
